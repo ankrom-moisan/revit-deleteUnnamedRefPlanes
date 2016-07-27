88fd2b2347e787002c781a03e7c6a1d152401153
derekwilfong <dkwilfong@gmail.com>
2016-07-27 09:51:46 -0700

    Add files via upload

---
+++ unnamedReferencePlanes/unnamedReferencePlanes/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+
+namespace unnamedReferencePlanes
+{
+    [Transaction (TransactionMode.Manual)]
+
+    class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            ElementCategoryFilter categoryfilter =
+                new ElementCategoryFilter(BuiltInCategory.OST_ReferenceLines);
+            FilteredElementCollector collector = new FilteredElementCollector(doc).WherePasses(categoryfilter);
+
+            //var query = from element in collector
+            //            where element.Name != null;
+
+            //ElementParameterFilter refName = new ElementParameterFilter(
+            //BuiltInParameter bip = new BuiltInParameter.DATUM_TEXT
+
+            //try
+            //{
+            //        foreach (Element e in collector)
+            //        {
+
+            //        }
+            throw new NotImplementedException();
+        }
+    }
+      
+    }
+}
